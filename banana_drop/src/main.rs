80868493df8900ecde6f824ed2c6d008598b33f8
Dewald Haasbroek <dewald844@gmail.com>
2025-04-25 13:53:28 +0200

    Move game state to own file

--- banana_drop/src/main.rs
+++ banana_drop/src/main.rs
@@ -4,79 +4,8 @@ use ruscii::drawing::{Pencil, RectCharset};
 use ruscii::keyboard::{KeyEvent, Key};
 use ruscii::spatial::{Vec2};
 use ruscii::gui::{FPSCounter};
-use rand::Rng;
 
-
-struct Bowl {
-    pub pos: Vec2
-}
-
-struct Banana {
-    pub pos: Vec2,
-    pub speed: f64,
-}
-
-struct GameState {
-    pub dimension: Vec2,
-    pub bowl: Bowl,
-    pub score: u32,
-    pub lives: u32,
-    pub level: u32,
-    pub bananas : Vec<Banana>,
-    pub frame_count : u32,
-}
-
-impl GameState {
-    pub fn new(size : Vec2) -> Self {
-        GameState {
-            dimension: Vec2::xy(size.x, size.y),
-            bowl: Bowl { pos: Vec2::xy(size.x/2, size.y-4) },
-            score: 0,
-            lives: 3,
-            level: 1,
-            bananas: Vec::new(),
-            frame_count: 0,
-        }
-    }
-
-    pub fn spawn_bananas(&mut self) {
-        let banana_count = self.level; // Number of bananas increases with level
-        let mut rng = rand::thread_rng();
-
-        for _ in 0..banana_count {
-            let x_pos = rng.gen_range(15..(self.dimension.x - 15)); // Random x position
-            self.bananas.push(Banana {
-                pos: Vec2::xy(x_pos, 5),  // Start under top horizontal line
-                speed: self.level as f64,       // Speed increases with level
-            });
-        }
-    }
-
-    pub fn update_bananas(&mut self) {
-        self.bananas.retain_mut(|banana| {
-            banana.pos.y += banana.speed as i32; // Move banana down
-            banana.pos.y < self.dimension.y      // Keep bananas within the screen
-        });
-    }
-
-    pub fn check_collisions(&mut self) {
-        self.bananas.retain(|banana| {
-            if (banana.pos.x >= self.bowl.pos.x && banana.pos.x < self.bowl.pos.x + self.dimension.x / 12)
-                && (banana.pos.y >= self.bowl.pos.y && banana.pos.y < self.bowl.pos.y + 2)
-            {
-                self.score += 1; // Increase score
-                false // Remove banana
-            } 
-            else if banana.pos.y >= (self.bowl.pos.y+4) { // banana reached the bottom
-                self.lives -= 1;
-                false
-            }
-            else {
-                true // Keep banana
-            }
-        });
-    }
-}
+mod game_state;
 
 fn is_right_most_edge(current_pos : Vec2) -> bool {
     if current_pos.x >= 16 {
@@ -98,7 +27,7 @@ fn main() {
     let mut fps_counter = FPSCounter::default();
     let mut app = App::default();
 
-    let mut game_state = GameState::new(app.window().size());
+    let mut game_state = game_state::game_state::GameState::new(app.window().size());
 
     app.run(|app_state: &mut State, window: &mut Window| {
 
